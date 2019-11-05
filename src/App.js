8488a5a7cb35142ba05c20b4660c1ee2df9f1e4b
unknown <beiker2203@gmail.com>
2019-11-05 02:30:01 -0500

    Comprobando solicitudes al servidor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api/test/test').then(response => {
+      console.log(response.data)
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+
   return (
     <div className="App">
       <Container>
